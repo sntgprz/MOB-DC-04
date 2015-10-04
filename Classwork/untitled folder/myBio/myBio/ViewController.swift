cf90e31db3e53d112a0a7c045b52df3b6dbbd2ba
sntgprz <sperez121@gmail.com>
2015-10-04 16:30:51 -0400

    Short Bio App (I Think)

---
+++ Classwork/untitled folder/myBio/myBio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myBio
+//
+//  Created by Santiago Perez on 9/30/15.
+//  Copyright (c) 2015 Santiago Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
