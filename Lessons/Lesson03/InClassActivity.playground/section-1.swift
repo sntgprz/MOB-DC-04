d5f37bf5a326c7c61ffcbcda93a4e5115ce20967
Tedi Konda <tedikonda@gmail.com>
2015-10-05 21:10:53 -0400

    Fixed countelements function to use characters.count

--- Lessons/Lesson03/InClassActivity.playground/section-1.swift
+++ Lessons/Lesson03/InClassActivity.playground/section-1.swift
@@ -7,7 +7,6 @@ import Foundation
 
 // TODO: Print "Hello {whatever the value of name is}, you are {whatever the value of age is} years old!"
 
-
 // TODO: Print “You can drink” below the above text if the user is above 21. If they are above 18, print “you can vote”. If they are above 16, print “You can drive”
 
 // TODO: Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
@@ -18,7 +17,7 @@ import Foundation
 
 // TODO: Print whether the above number is even
 
-// TODO: Print out "Hello {whatever the value of name is}, your name is {however long the string name is} characters long!. Use countElements()
+// TODO: Print out "Hello {whatever the value of name is}, your name is {however long the string name is} characters long!. Use string.characters.count
 
 // TODO: Print the sum of one hundred random numbers. Use rand() to generate random numbers.
 
