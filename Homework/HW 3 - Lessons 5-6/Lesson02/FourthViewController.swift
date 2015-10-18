46f6943d4d9260bb4aad91e812d842a351fc35a9
sntgprz <sperez121@gmail.com>
2015-10-18 15:44:35 -0400

    HW 3 DONE

--- Homework/HW 3 - Lessons 5-6/Lesson02/FourthViewController.swift
+++ Homework/HW 3 - Lessons 5-6/Lesson02/FourthViewController.swift
@@ -13,4 +13,61 @@ class FourthViewController: UIViewController {
     TODO seven: Hook up the text input box, label and and a ‘calculate’ button. Create a ‘fibonacci adder’ class with a method ‘fibonacciNumberAtIndex' which takes indexOfFibonacciNumber (an integer).  When the button is pressed, create an instance of that class, call the method, and print out the appropriate fibonacci number of an inputted integer.
     The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number.
 */
+    
+    
+    @IBOutlet weak var inputLabel: UITextField!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    @IBAction func calculateAction(sender: AnyObject) {
+        
+        //Check user's input for blanks
+        
+        if (inputLabel.text != "") {
+        
+            //Call my Fibonacci function to deliver the desired Fibonacci number
+            self.resultsLabel.text = String(fibonacciNth(Int(inputLabel.text!)!))
+        
+        }
+    }
+    
+    
+    
+    /*
+    
+    My old Fibonacci algorithm
+    
+    */
+    
+    func fibonacciNth (n: Int) ->Int {
+        
+        var a: Int = 0
+        var b: Int = 1
+        var c: Int = 0
+        
+        if n == 1 {
+            //Isolating the first number in Fibonacci makes my algorithm so much easier
+            return 0
+            
+        }else if (n > 1) {
+            
+            //Loop that adds up the "last" two numbers in the series to get the desired nth number
+            for _ in 2...n {
+                c = a + b
+                b = a
+                a = c
+            }
+            return c
+            
+        }else {
+            
+            //Wrong User Input
+            return 0
+            
+        }
+    }
+    
 }
+
+
+
--- Homework/HW 3 - Lessons 5-6/Lesson02/ThirdViewController.swift
+++ Homework/HW 3 - Lessons 5-6/Lesson02/ThirdViewController.swift
@@ -13,4 +13,34 @@ class ThirdViewController: UIViewController {
     TODO six: Hook up the number input text field, button and text label to this class. When the button is pressed, a message should be printed to the label indicating whether the number is even.
 
 */
+    
+    @IBOutlet weak var numberInputField: UITextField!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    @IBAction func calculateAction(sender: AnyObject) {
+        
+        
+        //Check the user's input
+        
+        if (numberInputField.text != "") {
+        
+            let residual = Int(numberInputField.text!)! % 2
+        
+            if residual == 0 {
+            
+                self.resultsLabel.text = "\(numberInputField.text!) is even."
+            
+            }else {
+            
+                self.resultsLabel.text = "\(numberInputField.text!) is odd."
+            
+        }
+        
+        }
+        
+        
+    }
+    
+   
 }
