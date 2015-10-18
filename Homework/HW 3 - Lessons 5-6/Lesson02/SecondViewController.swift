8be6b39497f9934ce7222daacf7c6886ae1044f8
sntgprz <sperez121@gmail.com>
2015-10-18 15:19:04 -0400

    HW3 View Controller 2

--- Homework/HW 3 - Lessons 5-6/Lesson02/SecondViewController.swift
+++ Homework/HW 3 - Lessons 5-6/Lesson02/SecondViewController.swift
@@ -11,4 +11,27 @@ import UIKit
 class SecondViewController: UIViewController {
 
     //TODO five: Display the cumulative sum of all numbers added every time the ‘add’ button is pressed. Hook up the label, text box and button to make this work.
+    
+    
+    @IBOutlet weak var numberField: UITextField!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+ 
+    @IBAction func addAction(sender: AnyObject) {
+        
+        //Check that the user's input is not blank
+        
+        if (numberField.text != "")  {
+        
+            let newNumber = Float(numberField.text!)!
+        
+            self.resultsLabel.text = String(
+            
+            Float(resultsLabel.text!)! + newNumber
+                
+            
+            )
+            
+        }
+    }
 }
