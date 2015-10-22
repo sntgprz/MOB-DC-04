a6c5b92116a28246736595bb0b0f4e362ec34a32
sntgprz <sperez121@gmail.com>
2015-10-21 20:02:58 -0400

    protocols first project

---
+++ Classwork/Lesson 8/delegationProject/delegationProject/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  delegationProject
+//
+//  Created by Santiago Perez on 10/21/15.
+//  Copyright © 2015 Santiago Perez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func editUsernameAction(sender: AnyObject) {
+    }
+
+}
+
