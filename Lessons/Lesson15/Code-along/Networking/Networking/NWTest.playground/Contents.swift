675588a94f5472693e38823ccf43e0bd6e5fbb54
Tedi Konda <tedikonda@gmail.com>
2015-11-18 21:17:25 -0500

    Added networking in class exercise.

---
+++ Lessons/Lesson15/Code-along/Networking/Networking/NWTest.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import XCPlayground
+
+// Let asynchronous code run
+XCPSetExecutionShouldContinueIndefinitely()
+
+
+//if let url = NSURL(string: "http://www.google.com") {
+//    // Step 2: create the task
+//    let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+//        // Do something with the data and see if there's an error
+//        if error == nil {
+//            let googleSource = NSString(data: data!, encoding: NSUTF8StringEncoding)
+//            print(googleSource)
+//        }else {
+//            print("There was an error talking to the google")
+//        }
+//    })
+//    // Step 3: resume/start task
+//    task.resume()
+//}
+
+
+
+
+
