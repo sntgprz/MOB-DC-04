d10a77aaf1c8a75cc66aa44d28300570973e3f13
sntgprz <sperez121@gmail.com>
2015-12-01 22:40:21 -0500

    unsure

---
+++ Homework/Final Project/XY Nosh Prototype/XY Nosh Prototype/TruckInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TruckInfoViewController.swift
+//  XY Nosh Prototype
+//
+//  Created by Scott Kaufman on 8/16/15.
+//  Copyright © 2015 Scott Kaufman. All rights reserved.
+//
+
+import UIKit
+
+class TruckInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
